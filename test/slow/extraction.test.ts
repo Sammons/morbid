7a2a6dabe2d711f76daa9b948ad3c60150d386f3
sammons <ben@sammons.io>
2019-01-19 16:57:52 -0500

    try to get extract to succeed on travis

--- test/slow/extraction.test.ts
+++ test/slow/extraction.test.ts
@@ -1,23 +1,26 @@
 import * as M from '../../src/index';
 import * as path from 'path';
 import { config as creds } from '../test-config';
-import { resetTestDatabase } from './test-utils';
+import { resetTestDatabase, cleanup } from './test-utils';
 import * as cp from 'child_process';
 import * as pg from 'pg';
 
 describe('morbid', () => {
+  beforeAll(async () => {
+    await resetTestDatabase('test_extract');
+  });
+  afterAll(async () => {
+    await cleanup();
+  });
   const name = 'morbid-test-output-definition';
   const testOutputFile = path.resolve(__dirname, name);
   test('extracting the test database schemas', async () => {
-    const dbName = 'test_extract';
-    // make sure db exists
-    await resetTestDatabase(dbName);
     // generate the outfile
     await M.Generate({
       destination: testOutputFile,
       pg: new pg.Pool({
         ...creds,
-        database: dbName,
+        database: 'test_extract',
       }),
       schemas: ['accounting', 'sys', 'tenant', 'media'],
     });

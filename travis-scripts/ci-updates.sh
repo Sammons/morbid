b136540409a0815b66c268caafbd041d8e106a39
sammons <ben@sammons.io>
2019-02-02 20:27:11 -0500

    try new updates again

--- travis-scripts/ci-updates.sh
+++ travis-scripts/ci-updates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ev
 BRANCH="$(git rev-parse --abbrev-ref HEAD)";
-
+echo on branch $BRANCH
 if [ "$BRANCH" = "master" ]; then
   echo bumping version
   node travis-scripts/increment-package-version.js
@@ -9,12 +9,13 @@ if [ "$BRANCH" = "master" ]; then
   git add coverage;
   git add package.json;
   git status;
-  echo $gpg_private > gpg_private.pem
-  echo $gpg_public > gpg_public.pem
-  echo $rsa_private > rsa_private.pem
+  echo $gpg_private | sed "s/\^/\n/g" > gpg_private.pem
+  echo $gpg_public | sed "s/\^/\n/g" > gpg_public.pem
+  echo $rsa_private | sed "s/\^/\n/g" > rsa_private.pem
   gpg --import --passphrase $gpg_pass gpg_private.pem
   gpg --import gpg_public.pem
   git config --global user.signingkey 0F2911F3E9978A33
   GIT_SSH_COMMAND="ssh -i travis-scripts/rsa_private.pem" git commit -S -m "travis updating coverage, version [ci skip]"
+  git push origin master;
 fi;
 
